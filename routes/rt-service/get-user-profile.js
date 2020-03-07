7f6eb6d627b277eb3e9dc6b9a0ad969a40f21a57
Seung Lee <seung.lee@fishnchipper.io>
2020-03-07 11:58:31 +1100

    minor change

--- routes/rt-service/get-user-profile.js
+++ routes/rt-service/get-user-profile.js
@@ -5,8 +5,7 @@
 function on(req, res, next) {
     
     let user = req.decodedSession;
-
-    let values = { userId: user.user_id, userImage:(user.picture)? user.picture: "/img/sign-in.png", userName: (user.name)?user.name:"N/A", userEmail:user.email, userEmailVerified: user.email_verified};
+    let values = { userId: user.user_id, userImage:(user.picture)? user.picture: "/img/sign-in.png", userName: (user.name)?user.name:user.email.split(/@(.+)/)[0], userEmail:user.email, userEmailVerified: user.email_verified};
 
     res.render('./service/profile', values, function(err, html) {
         if(err) {

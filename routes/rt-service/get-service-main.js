6315b90ee9b33b37677dc8f7cf11f14ef8da56cc
Seung Lee <seung.lee@fishnchipper.io>
2020-06-08 10:16:08 +1000

    minor change

--- routes/rt-service/get-service-main.js
+++ routes/rt-service/get-service-main.js
@@ -5,7 +5,7 @@
 function on(req, res, next) {
     
     //console.log(" ==== ", req.decodedSession);
-    let decodedSession = req.decodedSession
+    let decodedSession = req.decodedSession;
     let displayName = (decodedSession.name)? decodedSession.name : decodedSession.email;
     let values = {firstInitial: displayName.charAt(0).toUpperCase()};
 
